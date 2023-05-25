83ea575d6e8117d6df12f6c866fa8b25cce6cc62
Muhammad Awais <m.awais2030@gmail.com>
2023-05-25 14:54:23 +0500

    Added Course Model

--- Program.cs
+++ Program.cs
@@ -6,3 +6,10 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("/courses", () => {});
 
 app.Run();
+
+public class Course {
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public long Duration { get; set; }
+    public int Type { get; set; }
+}
